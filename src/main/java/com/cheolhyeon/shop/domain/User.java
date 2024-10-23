b714cfef17e0d05da1b7922b455c7fa6ba48f53b
박철현 <park82602@gmail.com>
2024-10-23 19:40:24 +0900

    Feat : User의 역할과 비밀번호를 암호화할 수 있는 메서드를 구현했습니다

--- src/main/java/com/cheolhyeon/shop/domain/User.java
+++ src/main/java/com/cheolhyeon/shop/domain/User.java
@@ -1,11 +1,13 @@
 package com.cheolhyeon.shop.domain;
 
+import com.cheolhyeon.shop.dto.SignUp;
 import com.cheolhyeon.shop.type.UserRole;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
 @NoArgsConstructor
@@ -22,4 +24,18 @@ public class User {
     private String email;
     private String phone;
     private UserRole role;
+
+    public static User fromRequest(SignUp.Request request) {
+        return User.builder()
+                .name(request.getName())
+                .password(request.getPassword())
+                .email(request.getEmail())
+                .phone(request.getPhone())
+                .build();
+    }
+
+    public void setUpRoleAndPassword(PasswordEncoder passwordEncoder, UserRole role) {
+        this.role = role;
+        this.password = passwordEncoder.encode(password);
+    }
 }
