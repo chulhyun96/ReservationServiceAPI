973cc7d1310da4d55442fba4122124d79b430c21
박철현 <park82602@gmail.com>
2024-10-23 18:58:29 +0900

    Feat : 회원 가입된 유저의 이름을 찾아올 기능을 구현했습니다

--- src/main/java/com/cheolhyeon/shop/repository/ManagerRepository.java
+++ src/main/java/com/cheolhyeon/shop/repository/ManagerRepository.java
@@ -2,8 +2,12 @@ package com.cheolhyeon.shop.repository;
 
 import com.cheolhyeon.shop.domain.Manager;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
+    Optional<Manager> findByUsername(String username);
 }
