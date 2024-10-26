4dd7c4548562ed09b16e28caaa3cc7cca4b92704
박철현 <park82602@gmail.com>
2024-10-26 19:45:50 +0900

    Feat : 회원가입시 발생하는 에러를 담당하는 에러 클래스를 만들었습니다

--- src/main/java/com/cheolhyeon/shop/exception/SignUpException.java
+++ src/main/java/com/cheolhyeon/shop/exception/SignUpException.java
@@ -1,9 +1,22 @@
 package com.cheolhyeon.shop.exception;
 
 import com.cheolhyeon.shop.type.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignUpException extends RuntimeException {
+    private ErrorCode errorCode;
+    private String message;
+
     public SignUpException(ErrorCode errorCode) {
         super(errorCode.getMessage());
+        this.errorCode = errorCode;
+        this.message = errorCode.getMessage();
     }
 }
